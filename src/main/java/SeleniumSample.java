88b3c7c1ff01cac008c24defa307751a4f6bbbfb
Biswanath Padhi <muna.padhi92@gmail.com>
2022-12-18 08:03:47 +0530

    1. added testng.xml 2. fixed mvn surefire issue of parallel test by updating the version 3. added gitignore for target

--- src/main/java/SeleniumSample.java
+++ src/main/java/SeleniumSample.java
@@ -1,8 +1,9 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.Test;
 
 public class SeleniumSample {
 
